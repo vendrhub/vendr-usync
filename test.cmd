6f06da3409f684b5a59788c656c78ec6d2233144
mattbrailsford <me@mattbrailsford.com>
2020-09-24 10:39:14 +0100

    Remove unused test file

--- test.cmd
+++
@@ -1,4 +0,0 @@
-for /f "usebackq tokens=*" %%i in (`.\build\_tools\vswhere -latest -requires Microsoft.Component.MSBuild -find MSBuild\**\Bin\MSBuild.exe`) do (
-  "%%i" "build\Build.proj  -target:%%BUILD_TARGET%%"
-  exit /b !errorlevel!
-)

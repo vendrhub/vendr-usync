3e50470888548d879abee68fe950879bc3bb05f0
mattbrailsford <me@mattbrailsford.com>
2021-03-17 09:56:22 +0000

    Added print template support

--- src/Vendr.uSync/VendrSyncComposer.cs
+++ src/Vendr.uSync/VendrSyncComposer.cs
@@ -22,6 +22,7 @@ namespace Vendr.uSync
             composition.Register<ISyncSerializer<CountryReadOnly>, CountrySerializer>();
 
             composition.Register<ISyncSerializer<EmailTemplateReadOnly>, EmailTemplateSerializer>();
+            composition.Register<ISyncSerializer<PrintTemplateReadOnly>, PrintTemplateSerializer>();
             composition.Register<ISyncSerializer<OrderStatusReadOnly>, OrderStatusSerializer>();
             composition.Register<ISyncSerializer<PaymentMethodReadOnly>, PaymentMethodSeralizer>();
 

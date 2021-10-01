85b594be5c76b8c4405e3c4d3d86c1a4a81b4cbd
mattbrailsford <me@mattbrailsford.com>
2021-10-01 11:21:54 +0100

    Fixed composer

--- src/Vendr.uSync/VendrSyncComposer.cs
+++ src/Vendr.uSync/VendrSyncComposer.cs
@@ -1,20 +1,31 @@
 ﻿#if NETFRAMEWORK
-
 using Umbraco.Core;
-using Vendr.Core.Models;
-using Vendr.uSync.Serializers;
-
 using Umbraco.Core.Composing;
+using Vendr.Core.Models;
+using Vendr.Umbraco;
+using Vendr.uSync.Serializers;
 using uSync8.BackOffice;
 using uSync8.Core;
 using uSync8.Core.Serialization;
+using IComposer = Umbraco.Core.Composing.IUserComposer;
+#else
+using uSync.BackOffice;
+using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.DependencyInjection;
+using Vendr.Extensions;
+using Vendr.Umbraco;
+#endif
 
 namespace Vendr.uSync
 {
-    [ComposeAfter(typeof(uSyncCoreComposer))]
     [ComposeBefore(typeof(uSyncBackOfficeComposer))]
-    public class VendrSyncComposer : IUserComposer
+#if NETFRAMEWORK
+    [ComposeAfter(typeof(uSyncCoreComposer))]
+    [ComposeAfter(typeof(VendrComposer))]
+#endif
+    public class VendrSyncComposer : IComposer
     {
+#if NETFRAMEWORK
         public void Compose(Composition composition)
         {
             composition.Register<ISyncSerializer<StoreReadOnly>, StoreSerializer>();
@@ -31,7 +42,15 @@ namespace Vendr.uSync
             composition.Register<ISyncSerializer<ShippingMethodReadOnly>, ShippingMethodSerializer>();
             composition.Register<ISyncSerializer<TaxClassReadOnly>, TaxClassSerializer>();
         }
+#else
+        public void Compose(IUmbracoBuilder builder)
+        {
+            // No need to register serializers in v9 as they
+            // are auto discovered however we do need to ensure
+            // that Vendr has been initialized so we'll call AddVendr
+            // which should auto escape if it's already been added
+            builder.AddVendr();
+        }
+#endif
     }
 }
-
-#endif

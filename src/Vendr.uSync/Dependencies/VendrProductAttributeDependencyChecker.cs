8ad159dd86be0b289e57c34ba5cb931a7880573a
Kevin Jump <kevin@thejumps.co.uk>
2022-10-07 17:21:13 +0100

    Add menu stuff for uSync Complete.

---
+++ src/Vendr.uSync/Dependencies/VendrProductAttributeDependencyChecker.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using Vendr.Core.Models;
+using uSync.Core.Dependency;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Extensions;
+
+namespace Vendr.uSync.Dependencies
+{
+    public class VendrProductAttributeDependencyChecker :
+        ISyncDependencyChecker<ProductAttributeReadOnly>
+    {
+        public UmbracoObjectTypes ObjectType => UmbracoObjectTypes.Unknown;
+
+        public IEnumerable<uSyncDependency> GetDependencies(ProductAttributeReadOnly item, DependencyFlags flags)
+            => new uSyncDependency
+            {
+                Name = item.Name,
+                Order = VendrConstants.Priorites.ProductAttributes,
+                Udi = Udi.Create(VendrConstants.UdiEntityType.ProductAttribute, item.Id)
+            }.AsEnumerableOfOne();
+    }
+}
